649afc11910857baf7ddf0b5d0a6a68dc42e1536
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-11-13 11:34:29 +0000

    Update docs after building Travis build 1330 of taurus-org/taurus
    The docs were built from the branch 'develop' against the commit
    e4e4205ab5829ce86d23d6b3ba1839b275c1aed5.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/taurus-org/taurus/jobs/611320513.
    
    The doctr command that was run is
    
        /home/travis/miniconda/bin/doctr deploy .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.6.3-alpha',
+    VERSION: '4.6.4-alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
